71dccf4c641cfbb2446f4899ca962573540e2d26
Vincent Agbuya <23sc4114_ms@psu.edu.ph>
2026-03-11 20:46:16 +0800

    Update: Enhanced hero background with gradient overlay effect

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"6c0baaebf70e0148f485f27d5616b3d3382da7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3577699871" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "958760805" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
